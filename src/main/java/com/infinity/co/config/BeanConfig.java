1ebe11611349233604756bae8f0b7b8004e27451
Kashyap <anant.kashyap99@gmail.com>
2021-01-10 20:33:55 +0530

    Adding user profile related changes.

--- src/main/java/com/infinity/co/config/BeanConfig.java
+++ src/main/java/com/infinity/co/config/BeanConfig.java
@@ -4,6 +4,7 @@ import org.apache.catalina.servlets.WebdavServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class BeanConfig {
@@ -13,5 +14,10 @@ public class BeanConfig {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebdavServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
